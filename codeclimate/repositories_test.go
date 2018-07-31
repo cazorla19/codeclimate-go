85cf160297f22dc0d220f57084436abec6dea7dc
cazorla19 <michael.chinkov@gmail.com>
2018-07-31 13:17:51 +0200

    Test creation of public Github repository

--- codeclimate/repositories_test.go
+++ codeclimate/repositories_test.go
@@ -53,3 +53,27 @@ func TestCreatePrivateRepository(t *testing.T) {
 
 	fmt.Printf(string(repoMarshaled))
 }
+
+func TestCreatePublicRepository(t *testing.T) {
+	token := os.Getenv("CODECLIMATE_API_TOKEN")
+	client, err := NewClient(token)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	privateRepo := false
+	repo, err2 := client.CreateGithubRepository("babbel-sandbox", "test.public.babbel", privateRepo)
+
+	if err2 != nil {
+		t.Fatal(err)
+	}
+
+	repoMarshaled, err3 := json.Marshal(repo)
+
+	if err3 != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Printf(string(repoMarshaled))
+}
